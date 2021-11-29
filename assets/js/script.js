53499962b67c676f6cca358cf617faae6711aec9
Ankur Shahi <ankurshahi@MacBook-Air-2.local>
2021-11-28 19:49:45 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let buttonEl = document.querySelector("#save-task");
+let taskToDoEl = document.querySelector("#tasks-to-do");
+
+let createTaskHandler = function() {
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent="This is a new task.";
+    taskToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click",createTaskHandler);
